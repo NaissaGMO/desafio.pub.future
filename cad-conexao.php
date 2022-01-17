128253d9b77b2e79aa4e06110678e150c2bad179
Gabriela <72226295+NaissaGMO@users.noreply.github.com>
2022-01-17 00:07:03 -0300

    Create cad-conexao.php

---
+++ cad-conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'pub.future-db';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
