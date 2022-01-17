ee776046c265ca2e249fe54e8c18262174190f0a
Gabriela <72226295+NaissaGMO@users.noreply.github.com>
2022-01-17 00:15:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset = "UTF-8">
+    <meta http-equiv = "X-UA-COMPATIBLE" content = "IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "styles.css">
+    <title>Página inicial</title>
+</head>
+<body>
+    <div id="login-container">
+        <h1>Contas</h1>
+        <a href="login.php" class="button"> <button class="btn1"> entrar </button> </a> 
+        <a href="cadastro.php" class="button"> <button class="btn1"> cadastrar </button> </a> 
+        <a href="contas.php" class="button"> <button class="btn1"> listar contas </button> </a>
+    </div>
+</body>
